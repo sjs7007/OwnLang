7bc47232dea87da5412eb1e89bab32a5591b878c
sjs7007 <sjs7007@gmail.com>
2014-04-05 23:10:17 +0530

    Temp function.

--- temp.c
+++ temp.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int add(int x,int y)
+{
+	int z = x+y;
+	return z;
+}
+
 int main(int argc, char** argv)
 {
 	puts("Hello World!\n");
+	int x=4,y=5;
+	printf("%d + %d = %d\n",x,y,add(x,y));
 	return 0;
 }
