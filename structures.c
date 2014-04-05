e085317507a6efeed7f171c9155bc592512457c7
sjs7007 <sjs7007@gmail.com>
2014-04-05 23:19:30 +0530

    Created array of structures.

--- structures.c
+++ structures.c
@@ -11,5 +11,12 @@ int main()
 	point p;
 	p.x=14;
 	p.y=56;
-	printf("Point P : (%f,%f)",p.x,p.y);
+	printf("Point P : (%f,%f)\n",p.x,p.y);
+	point pArray[4];
+	for(int i=0;i<4;i++)
+	{
+		pArray[i].x=i;
+		pArray[i].y=i;
+		printf("Point P[%d] : (%f,%f)\n",i,pArray[i].x,pArray[i].y);
+	}
 }
